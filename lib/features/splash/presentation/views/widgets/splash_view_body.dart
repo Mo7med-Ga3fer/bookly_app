ace7c6ce2e996379940e005b1e4a80dceac571ce
Mo7medGa3fer <Mo7medGa3fer@gmail.com>
2023-08-21 03:36:02 +0300

    adding animation to splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,34 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../home/presentation/views/home_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateHomeView();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,11 +36,23 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          'Read what you want',
-          textAlign: TextAlign.center,
-        )
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 12),end: Offset.zero).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateHomeView() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      (){
+        Get.to (() => const HomeView(), transition: Transition.fade, duration: kTransitionDuration);
+      }
+      );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,6 +16,7 @@ class BooklyApp extends StatelessWidget {
    return GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+    home: const SplashView(),
    );
   }
 }
